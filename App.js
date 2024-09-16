34eff6091897d368cab609b89e867805711a7710
ayandip paul <ayandippaul284@gmail.com>
2024-09-16 23:57:46 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Button } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={{ fontSize: 30 }}>SAYANI</Text>
+      <Button title='press here'></Button>
+      <Text style={{ fontSize: 30 }}>AYANDIP</Text>
+      <Button title="press here too"></Button>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App
