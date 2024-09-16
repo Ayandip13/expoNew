8bc6088e691c56e62af63db9a8bfd5829307538e
ayandip paul <ayandippaul284@gmail.com>
2024-09-17 03:15:54 +0530

    add jsx concepts

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
 
+const name = "Ayandip"
+function fruit() {
+  return "apple"
+}
+
 const App = () => {
+  const age = 46
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>SAYANI</Text>
-      <Button title='press here'></Button>
-      <Text style={{ fontSize: 30 }}>AYANDIP</Text>
-      <Button title="press here too"></Button>
+      <Text style={{ fontSize: 30 }}>Hey {name} you have logged in sucessfully, your age is {age} his favourite fr is {fruit()} {age > 18? "above 18" : "below 18"}</Text>
     </View>
   );
 };
