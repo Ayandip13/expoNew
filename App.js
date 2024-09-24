e844cd0ff5dae23ae0944bd4fdb46339cf02459a
ayandip paul <ayandippaul284@gmail.com>
2024-09-24 23:38:17 +0530

    add text-input

--- App.js
+++ App.js
@@ -1,37 +1,28 @@
-import React from "react";
-import { Text, View, Button, StyleSheet } from "react-native";
-import Exstyles from './style'
+import React, { useState } from "react";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 
 const App = () => {
-
+  const [name, setname] = useState("")
   return (
     <View>
-      <Text style={{ fontSize: 30, padding: 30, backgroundColor: 'red' }}>Applying styles in native</Text>
+      <Text style={{ fontSize: 40, padding: 30, backgroundColor: 'lightblue' }}>Text input in native</Text>
 
-      <Text style={styles.textBox}>Styles in React</Text>
-      <Text style={[Exstyles.textBox,{margin:'6'}]}>Styles in React</Text>
-      <Text style={styles.textBox}>Styles in React</Text>
-
-    </View>
+      <Text style={{fontSize:35}}>Your name :{name}</Text>
+      <TextInput style={styles.TextInput} value={name} onChangeText={(text)=>setname(text)} placeholder="Enter your name" />
+        <Button title='Clear text' onPress={()=>setname('')} />
+    </View> 
   );
 };
+
 const styles = StyleSheet.create({
-  textBox: {
-    borderRadius:9,
-    fontSize: 40,
-    backgroundColor:'gold',
-    padding: 35,
-    margin:10,
-    color: 'blue',
-    textAlignVertical:'center',
-    textAlign:'center',
-    borderColor:'black',
-    borderWidth:4
+  TextInput: { fontSize: 18,
+    color: 'red',
+    borderWidth: 3, 
+    borderColor: 'blue', 
+    margin: 5,
+    padding:3 
   }
 })
 
-
 export default App
-//here we use double object in style part.. because outer is for style object inner is for jsx and this type of using style is called inline style which is directly use inside a component
-//When we make style for one specific component and in same file that is called internal style, `create` is An identity function for creating style sheets.
 
